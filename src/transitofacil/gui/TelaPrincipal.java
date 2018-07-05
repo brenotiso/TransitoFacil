3d8d7c2ab3d244d37dfe32c6ac6c71ecc66bf99a
Matheus Bento Fernandes <matheusfernandes1981@hotmail.com>
2018-07-05 16:40:58 -0300

    adicionando TelaPrincipal de tamanho fixo e arrumando a localização dos componentes

--- src/transitofacil/gui/TelaPrincipal.java
+++ src/transitofacil/gui/TelaPrincipal.java
@@ -1,6 +1,7 @@
 package transitofacil.gui;
 
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -11,12 +12,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 public class TelaPrincipal extends JFrame {
 
     private GridBagConstraints gbc;
     private GridBagLayout gbl;
     private JPanel pnPrincipal;
+    private JLabel lbTitulo;
 
     public TelaPrincipal() {
         super("Trânsito Fácil");
@@ -39,7 +43,7 @@ public class TelaPrincipal extends JFrame {
     private void construirTela() {
         pnPrincipal = new JPanel(gbl);
         setContentPane(pnPrincipal);
-        JLabel lbTitulo = new JLabel("Aprendizado de Trânsito");
+         lbTitulo = new JLabel("Aprendizado de Trânsito");
         lbTitulo.setFont(new Font("", Font.BOLD, 28));
 
         JButton btPlacas = new JButton("Placas");
@@ -76,31 +80,38 @@ public class TelaPrincipal extends JFrame {
             }
         });
 
+        JSeparator spFooter = new JSeparator(SwingConstants.HORIZONTAL);
         JLabel lbGrupo1 = new JLabel("Software desenvolvido para a disciplina de PPOO, ministrada pelo professor Paulo Afonso.");
         JLabel lbGrupo2 = new JLabel("Breno Tana, Matheus Fernandes e Rodrigo Marafelli.");
         JLabel lbGrupo3 = new JLabel("UFLA - 2018");
 
         // Adicionando os componentes à tela
-        adicionarComponente(lbTitulo, GridBagConstraints.CENTER, GridBagConstraints.NONE, 0, 0, 1, 1);
-        adicionarComponente(btPlacas, GridBagConstraints.WEST, GridBagConstraints.NONE, 1, 0, 1, 1);
-        adicionarComponente(btTransitoGeral, GridBagConstraints.WEST, GridBagConstraints.NONE, 2, 0, 1, 1);
-        adicionarComponente(btSimuladoPlacas, GridBagConstraints.WEST, GridBagConstraints.NONE, 3, 0, 1, 1);
-        adicionarComponente(btSimuladoTransitoGeral, GridBagConstraints.WEST, GridBagConstraints.NONE, 4, 0, 1, 1);
-        adicionarComponente(lbGrupo1, GridBagConstraints.CENTER, GridBagConstraints.NONE, 5, 0, 1, 1);
-        adicionarComponente(lbGrupo2, GridBagConstraints.CENTER, GridBagConstraints.NONE, 6, 0, 1, 1);
-        adicionarComponente(lbGrupo3, GridBagConstraints.CENTER, GridBagConstraints.NONE, 7, 0, 1, 1);
+        adicionarComponente(lbTitulo, GridBagConstraints.PAGE_START, GridBagConstraints.BOTH, 0, 0, 1, 1, 3, 3, 3, 3);
+//        adicionarComponente(btPlacas, GridBagConstraints.WEST, GridBagConstraints.VERTICAL, 1, 0, 1, 1, 3, 3, 3, 3);
+//        adicionarComponente(btTransitoGeral, GridBagConstraints.WEST, GridBagConstraints.NONE, 2, 0, 1, 1, 0, 3, 3, 3);
+//        adicionarComponente(btSimuladoPlacas, GridBagConstraints.WEST, GridBagConstraints.NONE, 3, 0, 1, 1, 0, 3, 3, 3);
+//        adicionarComponente(btSimuladoTransitoGeral, GridBagConstraints.WEST, GridBagConstraints.NONE, 4, 0, 1, 1, 0, 3, 3, 3);
+//        adicionarComponente(spFooter, GridBagConstraints.CENTER, GridBagConstraints.BOTH, 5, 0, 1, 1, 0, 0, 3, 0);
+//        adicionarComponente(lbGrupo1, GridBagConstraints.CENTER, GridBagConstraints.NONE, 6, 0, 1, 1, 0, 3, 3, 3);
+//        adicionarComponente(lbGrupo2, GridBagConstraints.CENTER, GridBagConstraints.NONE, 7, 0, 1, 1, 0, 3, 3, 3);
+//        adicionarComponente(lbGrupo3, GridBagConstraints.CENTER, GridBagConstraints.NONE, 8, 0, 1, 1, 0, 0, 3, 0);
 
-        pack();
+        Dimension d = new Dimension(800, 600);
+        setSize(d);
+        //pack();
     }
 
-    private void adicionarComponente(Component comp, int anchor, int fill, int linha, int coluna, int larg, int alt) {
+    private void adicionarComponente(Component comp, int anchor, int fill, int linha, int coluna, int larg, int alt,
+            int top, int left, int bot, int right) {
         gbc.anchor = anchor; // posicionamento do componente na tela (esquerda, direita, centralizado, etc)
         gbc.fill = fill; // define se o tamanho do componente será expandido ou não
         gbc.gridy = linha; // linha do grid onde o componente será inserido
         gbc.gridx = coluna; // coluna do grid onde o componente será inserido
         gbc.gridwidth = larg; // quantidade de colunas do grid que o componente irá ocupar
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
         gbc.gridheight = alt; // quantidade de linhas do grid que o componente irá ocupar
-        gbc.insets = new Insets(3, 3, 3, 3); // espaçamento (em pixels) entre os componentes da tela
+        gbc.insets = new Insets(top, left, bot, right); // espaçamento (em pixels) entre os componentes da tela
         gbl.setConstraints(comp, gbc); // adiciona o componente "comp" ao layout com as restrições previamente especificadas
         pnPrincipal.add(comp); // efetivamente insere o componente na tela
     }
@@ -160,13 +171,13 @@ public class TelaPrincipal extends JFrame {
         });
 
         // Adicionando os componentes à tela
-        adicionarComponente(lbTitulo, GridBagConstraints.CENTER, GridBagConstraints.NONE, 0, 0, 1, 1);
-        adicionarComponente(btRegulamentacao, GridBagConstraints.WEST, GridBagConstraints.NONE, 1, 0, 1, 1);
-        adicionarComponente(btAdvertencia, GridBagConstraints.WEST, GridBagConstraints.NONE, 2, 0, 1, 1);
-        adicionarComponente(btIndicacao, GridBagConstraints.WEST, GridBagConstraints.NONE, 3, 0, 1, 1);
-        adicionarComponente(btAtrativos, GridBagConstraints.WEST, GridBagConstraints.NONE, 4, 0, 1, 1);
-        adicionarComponente(btSinalizacao, GridBagConstraints.WEST, GridBagConstraints.NONE, 5, 0, 1, 1);
-        adicionarComponente(btVoltar, GridBagConstraints.WEST, GridBagConstraints.NONE, 6, 0, 1, 1);
+//        adicionarComponente(lbTitulo, GridBagConstraints.CENTER, GridBagConstraints.NONE, 0, 0, 1, 1);
+//        adicionarComponente(btRegulamentacao, GridBagConstraints.WEST, GridBagConstraints.NONE, 1, 0, 1, 1);
+//        adicionarComponente(btAdvertencia, GridBagConstraints.WEST, GridBagConstraints.NONE, 2, 0, 1, 1);
+//        adicionarComponente(btIndicacao, GridBagConstraints.WEST, GridBagConstraints.NONE, 3, 0, 1, 1);
+//        adicionarComponente(btAtrativos, GridBagConstraints.WEST, GridBagConstraints.NONE, 4, 0, 1, 1);
+//        adicionarComponente(btSinalizacao, GridBagConstraints.WEST, GridBagConstraints.NONE, 5, 0, 1, 1);
+//        adicionarComponente(btVoltar, GridBagConstraints.WEST, GridBagConstraints.NONE, 6, 0, 1, 1);
 
         pack();
     }
